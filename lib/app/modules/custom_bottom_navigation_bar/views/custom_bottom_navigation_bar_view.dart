7ad97fc1cea019530a1774ec720ed9fc9615a3b5
arafat593 <arafathossain593@gmail.com>
2025-07-31 17:39:03 +0600

    add bottomnavigation

---
+++ lib/app/modules/custom_bottom_navigation_bar/views/custom_bottom_navigation_bar_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:tanit_tanit_app/app/data/app_colors.dart';
+import 'package:tanit_tanit_app/app/data/image_path.dart';
+
+import '../../all_search/views/all_search_view.dart';
+import '../../home/views/home_view.dart';
+import '../../platforms_link/views/platforms_link_view.dart';
+import '../../profile/views/profile_view.dart';
+import '../../start_now/views/start_now_view.dart';
+import '../controllers/custom_bottom_navigation_bar_controller.dart';
+
+class CustomBottomNavigationBarView
+    extends GetView<CustomBottomNavigationBarController> {
+  CustomBottomNavigationBarView({super.key});
+
+  final List _pages = [
+    HomeView(),
+    ProfileView(),
+    AllSearchView(),
+    PlatformsLinkView(),
+    StartNowView(),
+  ];
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Obx(
+      () => Scaffold(
+        body: _pages[controller.currentIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: controller.currentIndex.value,
+          onTap: controller.changeTab,
+          backgroundColor: Colors.transparent,
+          selectedItemColor: AppColors.darkBlue,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          unselectedItemColor: AppColors.gray,
+          items: [
+            _buildItem(0, ImagePath.house, 'Home'),
+            _buildItem(1, ImagePath.messageText, 'My Chatrooms'),
+            _buildItem(2, ImagePath.user, 'Profile'),
+            _buildItem(3, ImagePath.music, 'Albums'),
+            _buildItem(4, ImagePath.searchNormal, 'Search'),
+          ],
+        ),
+      ),
+    );
+  }
+  BottomNavigationBarItem _buildItem(int index, String iconPath, String label) {
+    final isSelected = controller.currentIndex.value == index;
+    return BottomNavigationBarItem(
+      icon: Image.asset(
+        iconPath,
+        height: 25,
+        width: 28,
+        color: isSelected ? AppColors.darkBlue : AppColors.gray,
+      ),
+      label: label,
+    );
+  }
+}
