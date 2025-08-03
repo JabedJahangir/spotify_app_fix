d5c7eaa0f8ed9755da89da44562b3813c7dd668f
arafat593 <arafathossain593@gmail.com>
2025-08-03 09:38:27 +0600

    update auth

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -16,6 +16,9 @@ abstract class Routes {
   static const PROFILE = _Paths.PROFILE;
   static const CUSTOM_BOTTOM_NAVIGATION_BAR =
       _Paths.CUSTOM_BOTTOM_NAVIGATION_BAR;
+  static const MY_CHAT_ROOM = _Paths.MY_CHAT_ROOM;
+  static const ALBUMS = _Paths.ALBUMS;
+  static const SEARCH = _Paths.SEARCH;
 }
 
 abstract class _Paths {
@@ -32,4 +35,7 @@ abstract class _Paths {
   static const ALL_SEARCH = '/all-search';
   static const PROFILE = '/profile';
   static const CUSTOM_BOTTOM_NAVIGATION_BAR = '/custom-bottom-navigation-bar';
+  static const MY_CHAT_ROOM = '/my-chat-room';
+  static const ALBUMS = '/albums';
+  static const SEARCH = '/search';
 }
